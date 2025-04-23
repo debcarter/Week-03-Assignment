f7fc408b00f78085104988f0dcad5b5747989dd2
Debora Carter <deborakcarter@gmail.com>
2025-04-22 20:40:28 -0600

    Revision1

--- src/pushtogithub/Codetopush.java
+++ src/pushtogithub/Codetopush.java
@@ -3,7 +3,8 @@ package pushtogithub;
 public class Codetopush {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		System.out.println("Have a great week!");
 
 	}
 
